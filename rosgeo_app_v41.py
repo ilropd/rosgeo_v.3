f901c2a1dd63061e5bed6623b48418eaac702ae9
ilropd <109026997+ilropd@users.noreply.github.com>
2022-11-07 14:21:25 +0300

    Update rosgeo_app_v41.py

--- rosgeo_app_v41.py
+++ rosgeo_app_v41.py
@@ -312,11 +312,17 @@ def preds_argmax_collectors(model='', x_test=''):
                 x_test[:,i] = x_test[:,i] - ficha[i]
             preds_collectors = model.predict(x_test)
             preds_collectors_noargmax = preds_collectors
+        else:
+            preds_collectors = model.predict(x_test)
+            preds_collectors_noargmax = preds_collectors
 
-        elif model is loaded_model_kononov_collectors:
+        if model is loaded_model_kononov_collectors:
             out_collectors = preds_collectors.astype(int)
+        else:
+            pred_args_collector = np.argmax(preds_collectors, axis=1)
+            out_collectors = args_to_types(pred_args_collector)
 
-        elif model is loaded_model_suslin_collectors:
+        if model is loaded_model_suslin_collectors:
             preds_20 = loaded_model_bagurin_collectors.predict(x_test)
             preds_collectors_noargmax = preds_20
             preds_20 = np.argmax(preds_20, axis=1)
@@ -328,7 +334,7 @@ def preds_argmax_collectors(model='', x_test=''):
             # убираем столбец, который не участвует в генераторе
             x_data = np.delete(x_test, 3, axis=1)
             x_data2 = get_x(x_data)
-
+            
             preds_collectors = model.predict(x_data2)
             preds_collectors_noargmax = preds_collectors
             pred_args_collector = np.argmax(preds_collectors, axis=1)
